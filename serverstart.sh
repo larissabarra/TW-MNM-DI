7d05f46ddd4024e38f18cef6f7c799d6380d2257
Larissa <lari_conde94@hotmail.com>
2017-10-06 15:06:49 -0300

    Update script to run server with logs

--- serverstart.sh
+++ serverstart.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+echo 'export app'
 export FLASK_APP=hello.py
+echo 'exported'
+echo 'starting server'
 flask run -h ec2-13-58-186-167.us-east-2.compute.amazonaws.com -p 50002
+echo 'server running'
