5e3663e44d904917e1575ad3dbf5c5e4c54634a0
Larissa <lari_conde94@hotmail.com>
2017-10-11 15:22:34 -0300

    Update server on shell script to run on 0.0.0.0

--- serverstart.sh
+++ serverstart.sh
@@ -4,5 +4,5 @@ echo "export app"
 export FLASK_APP=/home/lbarra/maonamassa/TW-MNM-DI/hello.py
 echo "exported"
 echo "starting server"
-flask run -h ec2-13-58-186-167.us-east-2.compute.amazonaws.com -p 50002
+flask run -h 0.0.0.0 -p 50002
 echo "server running"
