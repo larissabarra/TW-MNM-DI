49e84a2c0296bbf67b7b0c9f95294b3d37a5b94a
Larissa <lari_conde94@hotmail.com>
2017-10-17 18:15:51 -0200

    Change username in service and shell to test in vagrant

--- serverstart.sh
+++ serverstart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export FLASK_APP=/home/lbarra/maonamassa/TW-MNM-DI/hello.py
+export FLASK_APP=/home/vagrant/maonamassa/TW-MNM-DI/hello.py
 #touch /tmp/teste
 python3 -m flask run -h 0.0.0.0 -p 50002
 #python3 hello.py runserver -t 0.0.0.0 -p 50002
