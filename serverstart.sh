1eb8b50f027b8e0736c19de019847b952a122e14
root <root@ip-172-31-15-132.us-east-2.compute.internal>
2017-10-23 17:01:40 +0000

    Fix start server command to start in background

--- serverstart.sh
+++ serverstart.sh
@@ -4,5 +4,5 @@ export FLASK_APP=/home/lbarra/maonamassa/TW-MNM-DI/hello.py
 export LC_ALL=en_US.utf-8
 export LANG=en_US.utf-8
 #touch /tmp/teste
-python3 -m flask run -h 0.0.0.0 -p 50002
+python3 -m flask run -h 0.0.0.0 -p 50002 &
 #python3 hello.py runserver -t 0.0.0.0 -p 50002
